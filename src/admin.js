d193010150fbe04475f3ba26e60bb958bf8445d1
MessiStats <1247737960@qq.com>
2020-08-11 15:34:51 +0800

    导航栏实现

--- src/admin.js
+++ src/admin.js
@@ -2,14 +2,25 @@ import React from 'react'
 import {Row, Col} from 'antd'
 
 import Footer from './components/Footer/footer'
-import Header from './components/Header/Header'
+import Header from './components/Header/header'
+import NavLeft from './components/NavLeft/navLeft'
+import 'antd/dist/antd.css'
+import './components/style/common.less'
 
 class Admin extends React.Component {
     render() {
         return (
-            <Row>
-                <Col span="3">Left</Col>
-                <Col span="21">Right</Col>
+            <Row className="container">
+                <Col span={4} className="nav-left">
+                    <NavLeft/>
+                </Col>
+                <Col span={20} className="main">
+                    <Header/>
+                    <Row className="content">
+
+                    </Row>
+                    <Footer/>
+                </Col>
             </Row>
         )
     }
---
+++ src/components/NavLeft/navLeft.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {Menu} from 'antd';
+import menuList from '../../config/menuConfig'
+import './navLeft.css'
+import {MailOutlined, AppstoreOutlined} from '@ant-design/icons'
+
+const {SubMenu} = Menu
+
+class NavLeft extends React.Component {
+    state = {
+        menuTreeNode: []
+    }
+
+    componentWillMount() {
+        const menuTreeNode = this.renderMeun(menuList)
+        this.setState({
+            menuTreeNode
+        })
+    }
+
+    renderMeun = (data) => {
+        return data.map(item => {
+            if (item.children) {
+                return (
+                    <SubMenu key={item.key} title={item.title} icon={<MailOutlined />}>
+                        {this.renderMeun(item.children)}
+                    </SubMenu>
+                )
+            }
+            return <Menu.Item icon={<AppstoreOutlined />} key={item.key} title={item.title}>{item.title}</Menu.Item>
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="logo">
+                    <img src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg" alt=''/>
+                    <h1>MyBlog</h1>
+                </div>
+                <Menu>
+                    {this.state.menuTreeNode}
+                </Menu>
+            </div>
+        )
+    }
+}
+
+export default NavLeft
