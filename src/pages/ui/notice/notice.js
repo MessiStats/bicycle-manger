ddb9b5ed03565c163f895f9e8b3eab7a785c65df
MessiStats <1247737960@qq.com>
2020-08-15 11:55:54 +0800

    notice supply

--- src/pages/ui/notice/notice.js
+++ src/pages/ui/notice/notice.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Card, Button, notification} from 'antd'
+import {SmileOutlined} from '@ant-design/icons'
 
 class Notice extends React.Component {
 
     openNotification = (type, placement) => {
-        if (placement){
+        if (placement) {
             notification.config({
                 placement
             })
@@ -15,20 +16,32 @@ class Notice extends React.Component {
         })
     }
 
+    customOpenNotification = () => {
+        notification.open({
+            message: '发工资了',
+            description: '上个月考勤22天，迟到12天，实发工资250，请笑纳',
+            icon: <SmileOutlined style={{color: '#108ee9'}}/>
+        })
+    }
+
     render() {
         return (
             <div>
-                <Card title="通知提示框">
+                <Card title="带有图标的通知提示框">
                     <Button type="primary" onClick={() => this.openNotification('success')}>Success</Button>
                     <Button type="primary" onClick={() => this.openNotification('info')}>Info</Button>
                     <Button type="primary" onClick={() => this.openNotification('warning')}>Warning</Button>
                     <Button type="primary" onClick={() => this.openNotification('error')}>Error</Button>
                 </Card>
-                <Card title="通知提示框">
-                    <Button type="primary" onClick={() => this.openNotification('success','topLeft')}>Success</Button>
-                    <Button type="primary" onClick={() => this.openNotification('info','topRight')}>Info</Button>
-                    <Button type="primary" onClick={() => this.openNotification('warning','bottomLeft')}>Warning</Button>
-                    <Button type="primary" onClick={() => this.openNotification('error','bottomRight')}>Error</Button>
+                <Card title="切换位置提示框">
+                    <Button type="primary" onClick={() => this.openNotification('success', 'topLeft')}>Success</Button>
+                    <Button type="primary" onClick={() => this.openNotification('info', 'topRight')}>Info</Button>
+                    <Button type="primary"
+                            onClick={() => this.openNotification('warning', 'bottomLeft')}>Warning</Button>
+                    <Button type="primary" onClick={() => this.openNotification('error', 'bottomRight')}>Error</Button>
+                </Card>
+                <Card title="自定义图标提示框">
+                    <Button type="primary" onClick={this.customOpenNotification}>Open the notification box</Button>
                 </Card>
             </div>
         )
